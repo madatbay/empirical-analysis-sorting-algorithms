b3cb601612374d2787e23a76eccf37cef2f11298
Madat Bayramov <medet.bayramov22@gmail.com>
2024-12-21 21:56:14 +0400

    run each algorithm for every input value

--- main.py
+++ main.py
@@ -8,31 +8,34 @@ from utils.time_func import time_func
 
 
 def main():
-    arr = generate_random_array(100)
+    arr_100 = generate_random_array(100)
+    arr_1000 = generate_random_array(1000)
+    arr_10000 = generate_random_array(10000)
 
-    b_sorted, b_time = time_func(bubble_sort, arr)
-    i_sorted, i_time = time_func(insertion_sort, arr)
-    m_sorted, m_time = time_func(merge_sort, arr)
-    q_sorted, q_time = time_func(quick_sort, arr)
-    s_sorted, s_time = time_func(selection_sort, arr)
-
-    execution_durations = (
-        ("Bubble sort", b_time),
-        ("Insertion sort", i_time),
-        ("Merge sort", m_time),
-        ("Quick sort", q_time),
-        ("Selection sort", s_time),
+    sorting_functions = (
+        ("Bubble sort", bubble_sort),
+        ("Insertion sort", insertion_sort),
+        ("Merge sort", merge_sort),
+        ("Quick sort", quick_sort),
+        ("Selection sort", selection_sort),
     )
+
+    time_values = {}
+
+    # Run each sorting algorithm for every array value
+    for v in sorting_functions:
+        label, func = v
+        _, time_1 = time_func(func, arr_100)
+        _, time_2 = time_func(func, arr_1000)
+        _, time_3 = time_func(func, arr_10000)
+
+        time_values[label] = [time_1, time_2, time_3]
 
     print("Listing all sorting algorithms in sorted from the best to the worst:")
-    for index, value in enumerate(sorted(execution_durations, key=lambda x: x[1])):
-        name, time = value
-        print(index + 1, name, f": {time:.6f}")
-
-    print(
-        "\nAll sorted values equal: ",
-        b_sorted == i_sorted == m_sorted == q_sorted == s_sorted,
-    )
+    for idx, key in enumerate(time_values.keys()):
+        print(f"{idx + 1}.", f"{key}: ")
+        for idy, value in enumerate(time_values[key]):
+            print(f"\t{idy + 1}.", f"{value:.6f}")
 
 
 if __name__ == "__main__":
